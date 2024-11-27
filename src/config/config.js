b94d5ac52a478c1c418f7f26d524d447b44e41d8
Luis Miranda <lj.mirandag@icloud.com>
2024-11-26 21:02:53 -0300

    config

--- src/config/config.js
+++ src/config/config.js
@@ -3,9 +3,6 @@ const config = {
   ENVIRONMENT: process.env.ENVIRONMENT ?? 'local',
   PORT: process.env.PORT ?? 3000,
 
-  // BUSINESS
-  FREE_SUBSCRIPTION_LIMIT: 3,
-
   // MYSQL
   MYSQLHOST: process.env.MYSQLHOST ?? '127.0.0.1',
   MYSQLPORT: process.env.MYSQLPORT ?? 3306,
@@ -13,18 +10,19 @@ const config = {
   MYSQLPASSWORD: process.env.MYSQLPASSWORD ?? '',
   MYSQL_DATABASE: process.env.MYSQL_DATABASE ?? 'diario',
 
+  // JWT
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY ?? 'diarioAiepTarea',
+  JWT_EXPIRATION: process.env.JWT_EXPIRATION ?? '72h',
+
   // DB
   DB_STATUS_ACTIVE_ID: 1,
   DB_STATUS_INACTIVE_ID: 2,
   DB_ADMIN_ROLE_ID: 1,
   DB_JOURNALIST_ROLE_ID: 2,
 
-  // BCRYPT
-  BCRYPT_SALT: process.env.BCRYPT_SALT ?? 5,
-
-  // JWT
-  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY ?? 'diarioAiepTarea',
-  JWT_EXPIRATION: process.env.JWT_EXPIRATION ?? '72h'
+  // BUSINESS
+  FREE_SUBSCRIPTION_LIMIT: 3,
+  BCRYPT_SALT: 5
 };
 
 export default config;
