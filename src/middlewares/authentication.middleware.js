24eff181503a7f1e1cba74d03692535c4005b6b0
Andrea Castro <ap.castrocastellon@gmail.com>
2024-11-30 22:32:41 -0300

    middleware with id instead id_user on token verify

--- src/middlewares/authentication.middleware.js
+++ src/middlewares/authentication.middleware.js
@@ -2,12 +2,6 @@ import CONFIG from '../config/config.js';
 import jwt from '../shared/services/jwt.service.js';
 import { handlerHttpResponse } from '../shared/services/utils.service.js';
 
-const getAuthHeader = (headers) => {
-  const authHeader = headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  return token;
-}
-
 const setReqUser = (user, req, next) => {
   req.user = user;
   next();
@@ -63,11 +57,12 @@ const validateJournalistToken = (req, res, next) => { // El token del "headers.a
 
 const evaluateToken = (req) => {
   try {
-    const token = getAuthHeader(req.headers);
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
     if (!token)
       return { status: 401, user: null };
     const user = jwt.verifyToken(token, CONFIG.JWT_SECRET_KEY);
-    if (!user || !user.id_user || !user.id_person || !user.id_role)
+    if (!user || !user.id || !user.id_person || !user.id_role)
       return { status: 403, user };
     return { status: 200, user };
   } catch {
